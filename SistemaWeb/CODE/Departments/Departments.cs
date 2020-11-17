21aeb7c0119765a13b9ce68e509c6e4a73e99535
Eduardo <eduardotakeuchi@outlook.com>
2020-11-17 12:19:08 -0300

    Novo projeto

---
+++ SistemaWeb/CODE/Departments/Departments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaWeb.CODE
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+    }
+}
